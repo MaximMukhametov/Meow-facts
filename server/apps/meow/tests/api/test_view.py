208f6cb1f3bf087c1814fc6cfd260b69e89381ca
MaximMukhametov <viashino92@mail.ru>
2020-08-25 22:21:29 +0700

    add pip install to jenkinsfile

--- server/apps/meow/tests/api/test_view.py
+++ server/apps/meow/tests/api/test_view.py
@@ -1,2 +1,10 @@
-def test_view():
-    assert True
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+
+def test_get_users_view():
+    """Ensure anybody can see all users."""
+    url = reverse('run_task')
+    response = APIClient().get(url)
+
+    assert response.status_code == 200
